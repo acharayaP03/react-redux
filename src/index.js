84e850a66d8a0cc89d0cf0876f607cc7c278eb10
acharyaP03 <acharyap03@gmail.com>
2020-01-11 13:53:42 +1100

    Added .gitignore file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom'
 import routes from './routes'
 import sampleData from './initialState'
 import storeFactory from './store'
+import { Provider } from 'react-redux';
 
 const initialState = (localStorage["redux-store"]) ?
     JSON.parse(localStorage["redux-store"]) :
@@ -14,10 +15,13 @@ const saveState = () =>
 
 const store = storeFactory(initialState)
 window.React = React
+window.store = store
 //chain listener to the store..
 store.subscribe(saveState)
 
 render(
-	routes,
+  <Provider store={store}>
+    { routes }
+  </Provider>,
   document.getElementById('react-container')
 )
